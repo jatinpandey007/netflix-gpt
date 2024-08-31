3b1c1d03baa0e558426c87883904f2d9e71850a4
jatinpandey007 <jatin.pandey007@gmail.com>
2024-09-01 05:16:39 +0530

    Firebase env changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,14 +9,24 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCTmNuB0AVzK8kTb3sBqgYtDL07XpMt76c",
+  authDomain: "netflixgpt-d80b5.firebaseapp.com",
+  projectId: "netflixgpt-d80b5",
+  storageBucket: "netflixgpt-d80b5.appspot.com",
+  messagingSenderId: "373040704905",
+  appId: "1:373040704905:web:aeccdd394a897f7fb64558",
+  measurementId: "G-F0WKL9JR2L"
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
